8196adffd92921e6fa9215c65561972af02262c4
Nandraj <nandraj.rathod.009@gmail.com>
2020-05-08 13:12:02 +0530

    Addition and Substraction feature added

---
+++ app/view/v2/math/substract/math-substract-page.js
@@ -0,0 +1,45 @@
+var observableModule = require("tns-core-modules/data/observable");
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function ViewModel() {
+    var viewModel = observableModule.fromObject({
+        firstNum: 9,
+        symbol: "-",
+        secondNum: 6,
+        comment: "",
+        answer: "",
+        newSum: function () {
+            viewModel.firstNum = randomNumber(1, 9)
+            viewModel.secondNum = randomNumber(1, viewModel.firstNum)
+            viewModel.comment = ""
+            viewModel.answer = ""
+        },
+        showAns: function () {
+            let ans = eval(String(viewModel.firstNum) + "-" + String(viewModel.secondNum))
+            viewModel.answer = ans
+        },
+        checkAns: function () {
+            let ans = eval(String(viewModel.firstNum) + "-" + String(viewModel.secondNum))
+            if (String(viewModel.answer).length > 0) {
+                if (String(ans) === viewModel.answer) {
+                    viewModel.comment = "Correct! Very Good!"
+                } else {
+                    viewModel.comment = `Incorrect! correct answer is ${ans}`
+                }
+            } else {
+                viewModel.comment = "Invalid Input!"
+            }
+        }
+    })
+    return viewModel
+}
+
+function pageLoaded(args) {
+    const page = args.object
+    page.bindingContext = new ViewModel()
+}
+
+exports.pageLoaded = pageLoaded
